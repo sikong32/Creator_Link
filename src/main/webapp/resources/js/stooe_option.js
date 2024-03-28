24b9daae1f85798ecdf2442407bdf3acddb2f493
STD91 <sikong32@naver.com>
2024-03-28 13:08:41 +0900

    상품 등록 페이지 수정
    상품 등록 페이지 수정

--- src/main/webapp/resources/js/stooe_option.js
+++ src/main/webapp/resources/js/stooe_option.js
@@ -1,19 +1,74 @@
-var option1Count = 0;
-var option2Count = 0;
-var option3Count = 0;
-var option_delete_button_count = 0;
+var option1Count = 1;
+var option2Count = 1;
+var option3Count = 1;
+var optionCategoryCount = 0;
 
-function addOption1() {
-    option1Count++;
-    var container = document.getElementById('optionsContainer1');
+function addOptionCategory() { // 옵션 카테고리 추가
+	if(optionCategoryCount>2){
+		alert("옵션 카테고리 개수는 최대 3개까지 입니다.");
+	}else{
+    optionCategoryCount++; // 옵션 카테고리 누적
+    var container = document.getElementById('optionCategories'); // 만들 태그 위치 찾기
+    var div = document.createElement('div'); // 만들 태그
+    div.className = 'optionCategory'; // 태그 클래스 명
+    div.id = 'optionCategory_' + optionCategoryCount; // 태그 id 설정
+    // 만들 태그에 들어갈 내용
+    div.innerHTML = '옵션 카테고리 ' + optionCategoryCount +
+					'<li><div id="optionsContainer'+ optionCategoryCount +'"></div>' +
+					'<input type="button" onclick="addOption'+ optionCategoryCount +'()" value="옵션 추가"></li>';
+	var delete_Button = document.createElement('input'); // 만들 태그의 속성
+    delete_Button.type = "button"; // 만들 태그의 타입
+    delete_Button.value = '마지막 옵션 카테고리 제거'; // 만들 태그의 값
+    delete_Button.onclick = function() { // 만들 태그의 함수
+        OptionCategory_delete('optionCategory_' + optionCategoryCount); //옵션 카테고리 삭제 함수에 현재 선택된 옵션 카테로기 id 값 전송
+    	optionCategoryCount --; // 옵션 카테고리 누적 함수 -1
+    };
+    div.appendChild(delete_Button);// 옵션 카테고리에 삭제 버튼 추가
+    container.appendChild(div); // 옵션 카테고리 HTML 영역에 생성
+    
+	// 생성된 옵션 카테고리의 처음 1개의 옵션 생성
+    var container = document.getElementById('optionsContainer'+optionCategoryCount); 
     var div = document.createElement('div');
-    div.className = 'option1';
-    div.id= 'option1'+ option1Count;
+    div.className = 'option'+optionCategoryCount;
+    div.id= 'option1'+ optionCategoryCount;
+    div.innerHTML = '1옵션 이름: <input type="text" id="os_1name" name="os_1name" >' + 
+				    '1옵션 가격: <input type="text"	id="os_1price" name="os_1price" >' +
+				    '1옵션 사진: <input type="file"	id="os_1photo" name="os_1photo" >' +
+				    '1옵션 재고: <input type="number" min="0"	id="os_1stock" name="os_1stock" onchange="tot_stock()" class="stock">';
+    container.appendChild(div);
+	}
+}
+
+function OptionCategory_delete(categoryId) { // 옵션 카테고리 삭제 함수
+    var category = document.getElementById(categoryId);
+//     alert(categoryId);
+    if (category) {
+    	//각 옵션 카테고리의 ID를 찾아 옵션으 카운트를 초기화
+    	if(categoryId.slice(-1)==='1'){
+    		option1Count = 1;
+    	}else if(categoryId.slice(-1)==='2'){
+    		option2Count = 1;
+    	}else if(categoryId.slice(-1)==='3'){
+    		option3Count = 1;
+    	}
+        category.remove();//선택된 옵션 카테고리 삭제
+    }
+}
+function addOption1() { // 옵션 생성
+	if(option1Count>9){ // 최대 옵션 수 제한
+		alert("옵션 개수는 최대 10개까지 입니다.");
+	}else{
+	option1Count++; // 옵션 수 누적
+    var container = document.getElementById('optionsContainer1'); //옵션 생성 위치 파악
+    var div = document.createElement('div'); // 옵션 생성 태그 설정
+    div.className = 'option1'; // 옵션 태그 클래스명 설정
+    div.id= 'option1'+ option1Count; // 옵션 태그 ID명
+ 	// 옵션 태그 에 들어갈 내용
     div.innerHTML = option1Count +'옵션 이름: <input type="text" id="os_'+ option1Count +'name" name="os_'+ option1Count +'name" >' + 
 				    option1Count +'옵션 가격: <input type="text"	id="os_'+ option1Count +'price" name="os_'+ option1Count +'price" >' +
 				    option1Count +'옵션 사진: <input type="file"	id="os_'+ option1Count +'photo" name="os_'+ option1Count +'photo" >' +
-				    option1Count +'옵션 재고: <input type="number" min="0"	id="os_'+ option1Count +'stock" name="os_'+ option1Count +'stock" >';
-    var deleteButton = document.createElement('input');
+				    option1Count +'옵션 재고: <input type="number" min="0"	id="os_'+ option1Count +'stock" name="os_'+ option1Count +'stock" onchange="tot_stock()" class="stock">';
+    var deleteButton = document.createElement('input');//삭제 버튼 생성 input 태그로
     deleteButton.type = "button";
     deleteButton.value = '마지막 옵션 제거';
     deleteButton.onclick = function() {
@@ -22,8 +77,12 @@ function addOption1() {
     };
     div.appendChild(deleteButton);
     container.appendChild(div);
+	}
 }
-function addOption2() {
+function addOption2() { // 2번 옵션 카테고리에 옵션 생성 함수
+	if(option2Count>9){
+		alert("옵션 개수는 최대 10개까지 입니다.");
+	}else{
 	option2Count++;
     var container = document.getElementById('optionsContainer2');
     var div = document.createElement('div');
@@ -32,7 +91,7 @@ function addOption2() {
     div.innerHTML = option2Count +'옵션 이름: <input type="text" id="os_'+ option2Count +'name" name="os_'+ option2Count +'name" >' + 
 				    option2Count +'옵션 가격: <input type="text"	id="os_'+ option2Count +'price" name="os_'+ option2Count +'price" >' +
 				    option2Count +'옵션 사진: <input type="file"	id="os_'+ option2Count +'photo" name="os_'+ option2Count +'photo" >' +
-				    option2Count +'옵션 재고: <input type="number" min="0"	id="os_'+ option2Count +'stock" name="os_'+ option2Count +'stock" >' ;
+				    option2Count +'옵션 재고: <input type="number" min="0"	id="os_'+ option2Count +'stock" name="os_'+ option2Count +'stock" onchange="tot_stock()" class="stock">' ;
 	var deleteButton = document.createElement('input');
     deleteButton.type = "button";
     deleteButton.value = '마지막 옵션 제거';
@@ -42,8 +101,12 @@ function addOption2() {
     };
     div.appendChild(deleteButton);
     container.appendChild(div);
+	}
 }
-function addOption3() {
+function addOption3() {// 3번 옵션 카테고리에 옵션 생성 함수
+	if(option3Count>9){
+		alert("옵션 개수는 최대 10개까지 입니다.");
+	}else{
     option3Count++;
     var container = document.getElementById('optionsContainer3');
     var div = document.createElement('div');
@@ -52,7 +115,7 @@ function addOption3() {
     div.innerHTML = option3Count +'옵션 이름: <input type="text" id="os_'+ option3Count +'name" name="os_'+ option3Count +'name" >' + 
 				    option3Count +'옵션 가격: <input type="text"	id="os_'+ option3Count +'price" name="os_'+ option3Count +'price" >' +
 				    option3Count +'옵션 사진: <input type="file"	id="os_'+ option3Count +'photo" name="os_'+ option3Count +'photo" >' +
-				    option3Count +'옵션 재고: <input type="number" min="0"	id="os_'+ option3Count +'stock" name="os_'+ option3Count +'stock" >' ;
+				    option3Count +'옵션 재고: <input type="number" min="0"	id="os_'+ option3Count +'stock" name="os_'+ option3Count +'stock" onchange="tot_stock()" class="stock">' ;
 	var deleteButton = document.createElement('input');
     deleteButton.type = "button";
     deleteButton.value = '마지막 옵션 제거';
@@ -62,44 +125,28 @@ function addOption3() {
     };
     div.appendChild(deleteButton);
     container.appendChild(div);
+	}
 }
-
-function removeOption(id) {
+//옵션 삭제 버튼
+function removeOption(id) { 
 		var option = document.getElementById(id);
 		if(option){
 			option.remove();
 		}
 }
+// 옵션 재고 실시간 전체 재고에 반영
+function tot_stock() {
+	// 모든 os_stock 요소를 가져옴
+    var os_stocks = document.getElementsByClassName('stock');
+    // 총 재고를 저장할 변수 초기화
+    var totstock = 0;
 
-var optionCategoryCount = 0;
-
-function addOptionCategory() {
-	if(optionCategoryCount>2){
-		alert("옵션 개수는 최대 3개까지 입니다.");
-	}else{
-    optionCategoryCount++;
-    var container = document.getElementById('optionCategories');
-    var div = document.createElement('div');
-    div.className = 'optionCategory';
-    div.id = 'optionCategory_' + optionCategoryCount;
-    div.innerHTML = '옵션 부류 ' + optionCategoryCount +
-					'<li><div id="optionsContainer'+ optionCategoryCount +'"></div>' +
-					'<input type="button" onclick="addOption'+ optionCategoryCount +'()" value="옵션 추가"></li>';
-	var delete_Button = document.createElement('input');
-    delete_Button.type = "button";
-    delete_Button.value = '마지막 옵션 부류 제거';
-    delete_Button.onclick = function() {
-        OptionCategory_delete('optionCategory_' + optionCategoryCount);
-    	optionCategoryCount --;
-    };
-    div.appendChild(delete_Button);
-    container.appendChild(div);
+    // 각 요소의 값을 합산
+    for (var i = 0; i < os_stocks.length; i++) {
+	    var element = os_stocks[i];
+	    var value = parseInt(element.value) || 0; // 요소의 값을 정수로 변환 (값이 숫자가 아니거나 없으면 0으로 설정됨)
+	    totstock += value; // 총 재고에 해당 요소의 값을 더함
 	}
-}
-
-function OptionCategory_delete(categoryId) {
-    var category = document.getElementById(categoryId);
-    if (category) {
-        category.remove();
-    }
+    // 재고의 총합을 계산하여 pd_stock 요소의 값으로 설정
+    document.getElementById('pd_stock').value = totstock;
 }
