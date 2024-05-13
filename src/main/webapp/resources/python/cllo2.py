d7f12ccebb8a52a949a53cc0c7598620fc4cfaf7
jsy <seokyoung0924@gmail.com>
2024-05-13 17:49:39 +0900

    크롤링 수다 음악 고전게임 종합게임

--- src/main/webapp/resources/python/cllo2.py
+++ src/main/webapp/resources/python/cllo2.py
@@ -47,11 +47,12 @@ try:
             try:
                 vido = i.find_element(By.CLASS_NAME,"video_card_thumbnail__QXYT8").find_element(By.TAG_NAME,"img").get_attribute("src")
             except NoSuchElementException:
+                print("방송 이미지 오류")
                 vido = "1.jpg"  # 기본 이미지 경로
             image_src = i.find_element(By.CLASS_NAME, "video_card_profile__QHbN7").find_element(By.TAG_NAME,"img").get_attribute("src")
             live = int(i.find_element(By.CLASS_NAME, "video_card_badge__w02UD").text.replace('명 시청', '').replace(',', ''))
             name = i.find_element(By.CLASS_NAME, "name_text__yQG50").text
-            #c.execute("insert into BROADCAST_TBL values(BC_NUMBER_SQL.nextval,:1,:2,:3,:4,:5,:6)",(link,vido,image_src,name,live,"치지직"))
+            c.execute("insert into BROADCAST_TBL values(BC_NUMBER_SQL.nextval,:1,:2,:3,:4,:5,:6)",(link,vido,image_src,name,live,"치지직_메인"))
             print(link)
             print(vido)
             print(image_src)
@@ -80,7 +81,7 @@ try:
         vido = items[i].find_element(By.TAG_NAME,"img").get_attribute("src")
         imag = items[i].find_element(By.CSS_SELECTOR,"div.cBox-info > a > img").get_attribute("src")
         name = items[i].text
-        #c.execute("insert into BROADCAST_TBL values(BC_NUMBER_SQL.nextval,:1,:2,:3,:4,:5,:6)",(link,vido,imag,name.split('\n', 1)[0],int(saram.replace(',', '')),"아프리카"))
+        c.execute("insert into BROADCAST_TBL values(BC_NUMBER_SQL.nextval,:1,:2,:3,:4,:5,:6)",(link,vido,imag,name.split('\n', 1)[0],int(saram.replace(',', '')),"아프리카_메인"))
         print(link)
         print(vido)
         print(imag)
@@ -88,24 +89,26 @@ try:
         print(name.split('\n', 1)[0])
 except Exception as e:
     print("오류 발생:", e)
+print("여기까지는 아프리카메인")
 
 #치지직 수다
 driver.get("https://chzzk.naver.com/category/ETC/talk")
 time.sleep(2)
 try:
     # 인기 방송 중인 클래스 하위에 있는 요소를 찾습니다.
-    items = driver.find_elements(By.CLASS_NAME, "component_item__+48tj")
+    items = driver.find_elements(By.CLASS_NAME, "component_item__\+48tj")
     # 찾은 모든 요소에 대해 반복합니다.
     for i in range(1,11):
         link = items[i].find_element(By.CLASS_NAME,"video_card_thumbnail__QXYT8").get_attribute("href")
         saram = items[i].find_element(By.CLASS_NAME,"video_card_badge__w02UD").text.replace('명 시청', '').replace(',', '')
         try:
-            vido = items[i].find_element(By.TAG_NAME,"video_card_thumbnail__QXYT8").find_element(By.TAG_NAME,"img").get_attribute("src")
+            vido = items[i].find_element(By.CLASS_NAME,"video_card_image__yHXqv").get_attribute("src")
         except NoSuchElementException:
-                        vido = "1.jpg"  # 기본 이미지 경로
+            print("방송 이미지 오류")
+            vido = "1.jpg"  # 기본 이미지 경로
         imag = items[i].find_element(By.CLASS_NAME,"video_card_profile__QHbN7").find_element(By.TAG_NAME,"img").get_attribute("src")
         name = items[i].find_element(By.CLASS_NAME,"name_text__yQG50").text
-        #c.execute("insert into BROADCAST_TBL values(BC_NUMBER_SQL.nextval,:1,:2,:3,:4,:5,:6)",(link,vido,imag,name.split('\n', 1)[0],int(saram.replace(',', '')),"아프리카"))
+        c.execute("insert into BROADCAST_TBL values(BC_NUMBER_SQL.nextval,:1,:2,:3,:4,:5,:6)",(link,vido,imag,name,saram,"치지직_수다"))
         print(link)
         print(vido)
         print(imag)
@@ -113,8 +116,121 @@ try:
         print(name)
 except Exception as e:
     print("오류 발생:", e)
+print("여기까지는 치치직 수다")
+
+#치지직 음악
+driver.get("https://chzzk.naver.com/category/ETC/music")
+time.sleep(2)
+try:
+    # 인기 방송 중인 클래스 하위에 있는 요소를 찾습니다.
+    items = driver.find_elements(By.CLASS_NAME, "component_item__\+48tj")
+    print(len(items))
+    # 찾은 모든 요소에 대해 반복합니다.
+    for i in range(1,11):
+        link = items[i].find_element(By.CLASS_NAME,"video_card_thumbnail__QXYT8").get_attribute("href")
+        saram = items[i].find_element(By.CLASS_NAME,"video_card_badge__w02UD").text.replace('명 시청', '').replace(',', '')
+        try:
+            vido = items[i].find_element(By.CLASS_NAME,"video_card_image__yHXqv").get_attribute("src")
+        except NoSuchElementException:
+            print("방송 이미지 오류")
+            vido = "1.jpg"  # 기본 이미지 경로
+        imag = items[i].find_element(By.CLASS_NAME,"video_card_profile__QHbN7").find_element(By.TAG_NAME,"img").get_attribute("src")
+        name = items[i].find_element(By.CLASS_NAME,"name_text__yQG50").text
+        c.execute("insert into BROADCAST_TBL values(BC_NUMBER_SQL.nextval,:1,:2,:3,:4,:5,:6)",(link,vido,imag,name,saram,"치지직_음악"))
+        print(link)
+        print(vido)
+        print(imag)
+        print(saram)
+        print(name)
+except Exception as e:
+    print("오류 발생:", e)
+print("여기까지는 치치직 음악")
+
+#치지직 종합게임
+driver.get("https://chzzk.naver.com/category/ETC/various_games")
+time.sleep(2)
+try:
+    # 인기 방송 중인 클래스 하위에 있는 요소를 찾습니다.
+    items = driver.find_elements(By.CLASS_NAME, "component_item__\+48tj")
+    print(len(items))
+    # 찾은 모든 요소에 대해 반복합니다.
+    for i in range(1,11):
+        link = items[i].find_element(By.CLASS_NAME,"video_card_thumbnail__QXYT8").get_attribute("href")
+        saram = items[i].find_element(By.CLASS_NAME,"video_card_badge__w02UD").text.replace('명 시청', '').replace(',', '')
+        try:
+            vido = items[i].find_element(By.CLASS_NAME,"video_card_image__yHXqv").get_attribute("src")
+        except NoSuchElementException:
+            print("방송 이미지 오류")
+            vido = "1.jpg"  # 기본 이미지 경로
+        imag = items[i].find_element(By.CLASS_NAME,"video_card_profile__QHbN7").find_element(By.TAG_NAME,"img").get_attribute("src")
+        name = items[i].find_element(By.CLASS_NAME,"name_text__yQG50").text
+        c.execute("insert into BROADCAST_TBL values(BC_NUMBER_SQL.nextval,:1,:2,:3,:4,:5,:6)",(link,vido,imag,name,saram,"치지직_종합게임"))
+        print(link)
+        print(vido)
+        print(imag)
+        print(saram)
+        print(name)
+except Exception as e:
+    print("오류 발생:", e)
+print("여기까지는 치치직 종합게임")
+
+#치지직 고전 게임
+driver.get("https://chzzk.naver.com/category/ETC/classic_game")
+time.sleep(2)
+try:
+    # 인기 방송 중인 클래스 하위에 있는 요소를 찾습니다.
+    items = driver.find_elements(By.CLASS_NAME, "component_item__\+48tj")
+    print(len(items))
+    # 찾은 모든 요소에 대해 반복합니다.
+    for i in range(1,11):
+        link = items[i].find_element(By.CLASS_NAME,"video_card_thumbnail__QXYT8").get_attribute("href")
+        saram = items[i].find_element(By.CLASS_NAME,"video_card_badge__w02UD").text.replace('명 시청', '').replace(',', '')
+        try:
+            vido = items[i].find_element(By.CLASS_NAME,"video_card_image__yHXqv").get_attribute("src")
+        except NoSuchElementException:
+            print("방송 이미지 오류")
+            vido = "1.jpg"  # 기본 이미지 경로
+        imag = items[i].find_element(By.CLASS_NAME,"video_card_profile__QHbN7").find_element(By.TAG_NAME,"img").get_attribute("src")
+        name = items[i].find_element(By.CLASS_NAME,"name_text__yQG50").text
+        c.execute("insert into BROADCAST_TBL values(BC_NUMBER_SQL.nextval,:1,:2,:3,:4,:5,:6)",(link,vido,imag,name,saram,"치지직_고전게임"))
+        print(link)
+        print(vido)
+        print(imag)
+        print(saram)
+        print(name)
+except Exception as e:
+    print("오류 발생:", e)
+print("여기까지는 치치직 고전게임")
+
+#치지직 아트
+driver.get("https://chzzk.naver.com/category/ETC/art")
+time.sleep(2)
+try:
+    # 인기 방송 중인 클래스 하위에 있는 요소를 찾습니다.
+    items = driver.find_elements(By.CLASS_NAME, "component_item__\+48tj")
+    print(len(items))
+    # 찾은 모든 요소에 대해 반복합니다.
+    for i in range(1,11):
+        link = items[i].find_element(By.CLASS_NAME,"video_card_thumbnail__QXYT8").get_attribute("href")
+        saram = items[i].find_element(By.CLASS_NAME,"video_card_badge__w02UD").text.replace('명 시청', '').replace(',', '')
+        try:
+            vido = items[i].find_element(By.CLASS_NAME,"video_card_image__yHXqv").get_attribute("src")
+        except NoSuchElementException:
+            print("방송 이미지 오류")
+            vido = "1.jpg"  # 기본 이미지 경로
+        imag = items[i].find_element(By.CLASS_NAME,"video_card_profile__QHbN7").find_element(By.TAG_NAME,"img").get_attribute("src")
+        name = items[i].find_element(By.CLASS_NAME,"name_text__yQG50").text
+        c.execute("insert into BROADCAST_TBL values(BC_NUMBER_SQL.nextval,:1,:2,:3,:4,:5,:6)",(link,vido,imag,name,saram,"치지직_아트"))
+        print(link)
+        print(vido)
+        print(imag)
+        print(saram)
+        print(name)
+except Exception as e:
+    print("오류 발생:", e)
+print("여기까지는 치치직 아트")
 
 connect.commit()
 # 브라우저 종료
 driver.quit()
-print("여기까지는 치지직 수다")
+print("프로그램 종료")
